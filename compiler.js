d4c242aa6351f8c23ad7cb44b41dc8ede0c005f8
Mohd Irtefa <irtefa1@illinois.edu>
2013-07-17 00:54:23 -0700

    added main thread to check opcode in each line

--- compiler.js
+++ compiler.js
@@ -1,17 +1,62 @@
-function incorrectOpcode(opcode) {
-    var validOpcodes = ["add", "addi", "addiu", "addu", "and", "andi", "beq", "bne", "j", "jal", "jr", "lbu", "lhu", "ll", "lui", "lw", "nor", "or", "ori", "slt", "slti", "sltiu", "sll", "srl", "sb", "sc", "sh", "sw", "sub", "subu"];
+/*
+ * @param content {string} entire code
+ * @return _contentList {list} list of lines of code appended with their line numbers
+ */
+
+function enumerateLines(content) {
+    // split content based on newlines
+    var _contentList = content.split("\n");
+    var _lineNum = 1;
+    
+    // add line numbers to content
+    for (var i in _contentList) {
+        _contentList[_lineNum - 1] = _contentList[i] + " " + _lineNum;
+        _lineNum++;
+    }
+
+    return _contentList;
+};
+
+/*
+ * @param opcode {string} opcode 
+ * @return true/ false {boolean} invalid opcode
+ */
+
+function invalidOpcode(opcode) {
+    var _validOpcodes = ["add", "addi", "addiu", "addu", "and", "andi", "beq", "bne", "j", "jal", "jr", "lbu", "lhu", "ll", "lui", "lw", "nor", "or", "ori", "slt", "slti", "sltiu", "sll", "srl", "sb", "sc", "sh", "sw", "sub", "subu"];
     // check if given opcode is present in valid opcode list
-    if (validOpcodes.indexOf(opcode) >= 0) {
+    if (_validOpcodes.indexOf(opcode) >= 0) {
         return false;
     } else {
         return true;
     }
 };
 
+/*
+ * @param line {string} single line of code
+ * @return true/ false {boolean} syntax error
+ */
+
 function thereIsSyntaxError(line) {
-    if (incorrectOpcode(line.split(" ")[0]) === true) {
-        console.log("this line: " + line + "has the wrong opcode");
+    // check for invalid opcodes
+    if (invalidOpcode(line.split(" ")[0]) === true) {
+        var _words = line.split(" ");
+        console.log("wrong opcode at line " + _words[_words.length - 1]);
         return true;
     }
     return false;
 };
+
+/*
+ * @param content {string} all the code
+ */
+
+function runCompiler(content) {
+    var _lines = enumerateLines(content);
+
+    for (var i in _lines) {
+        if (thereIsSyntaxError(_lines[i]) === false) {
+            console.log("correct");
+        }
+    }
+}
